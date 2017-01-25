4e0dacb640cda34dbc9020a18c13747de0de42ec
jverbosky <jverbosk@gmail.com>
2017-01-25 11:47:09 -0500

    tweaked test formatting

--- test_addition.rb
+++ test_addition.rb
@@ -6,16 +6,18 @@ require_relative "addition.rb"
 class TestAddition < Minitest::Test
 
 	def test_1_plus_1_equals_2
-		#results = add(1, 1)  # Another way to seed test
-		assert_equal(2, add(1, 1))
+		results = add(1, 1)
+		assert_equal(2, results)
 	end
 
 	def test_2_plus_3_equals_5
-		assert_equal(5, add(2, 3))
+		results = add(2, 3)
+		assert_equal(5, results)
 	end
 
 	def test_3_fail_on_non_numbers
-		assert_equal(false, add("a", 3))
+		results = add("a", 3)
+		assert_equal(false, results)
 	end
 
 
