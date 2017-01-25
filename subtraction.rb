d2a05cb97127d6f25a5c16e305004657a3052b7b
jverbosky <jverbosk@gmail.com>
2017-01-25 12:27:37 -0500

    added logic for non-numeric values

--- subtraction.rb
+++ subtraction.rb
@@ -1,7 +1,12 @@
 def subtract(num_1, num_2)
-	return (num_1 - num_2).round(2)
+	if (num_1.is_a? Numeric) && (num_2.is_a? Numeric)
+		return (num_1 - num_2).round(2)
+	else
+		return false
+	end
 end
 
 
 # print subtract(3, 2)
 # print subtract(7.1, 3.18)
+print subtract("string", 5)
