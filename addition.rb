834b8217cce03a846b47abbf95f3cddb2b1cc8eb
jverbosky <jverbosk@gmail.com>
2017-01-25 15:11:44 -0500

    add test & function working with multiple addends, no test for strings

--- addition.rb
+++ addition.rb
@@ -1,5 +1,6 @@
 # Function to add two numbers
 
+# Early version
 # def add(num_1, num_2)
 # 	if (num_1.is_a? Numeric) && (num_2.is_a? Numeric)
 # 		return (num_1 + num_2 + additional).round(2)
@@ -11,14 +12,12 @@
 def add(*number)
 	sum = 0
 	number.each do |n|
-		# if n.is_a? Numeric
-			sum += n
-		# end
+		sum += n
 	end
 	return sum
 end
 
-print add(2, 3, 5)
+# print add(2, 3, 5, 6, 7, 8, 100, 2, 4, 2)
 # print add(8.3, 9.7)
 # print add("a", 3)
 # print add("a","b")
