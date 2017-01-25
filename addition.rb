8c3131a65f42842128e942d5123a152e56105bc8
jverbosky <jverbosk@gmail.com>
2017-01-25 12:16:31 -0500

    added test and function for basic subtraction

--- addition.rb
+++ addition.rb
@@ -2,11 +2,18 @@
 
 def add(num_1, num_2)
 	if (num_1.is_a? Numeric) && (num_2.is_a? Numeric)
-		return num_1 + num_2
+		return (num_1 + num_2).round(2)
 	else
 		return false
 	end
 end
 
+def subtract(num_1, num_2)
+	return num_1 - num_2
+end
+
 # print add(2, 3)
+# print add(8.3, 9.7)
 # print add("a", 3)
+# print add("a","b")
+# print subtract(3, 2)
--- test_addition.rb
+++ test_addition.rb
@@ -15,10 +15,24 @@ class TestAddition < Minitest::Test
 		assert_equal(5, results)
 	end
 
-	def test_3_fail_on_non_numbers
-		results = add("a", 3)
+	def test_3_adding_floats
+		results = add(1.3, 4.5)
+		assert_equal(5.8, results)
+	end
+
+	def test_5_adding_floats_with_varying_number_of_decimals
+		results = add(1.3, 4.51)
+		assert_equal(5.81, results)
+	end
+
+	def test_6_fail_on_non_numbers
+		results = add("a", "b")
 		assert_equal(false, results)
 	end
 
+	def test_7_1_minus_1_equals_0
+		results = subtract(1, 1)
+		assert_equal(0, results)
+	end
 
 end
