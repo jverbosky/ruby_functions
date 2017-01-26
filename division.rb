266aac44e92f6cc6701c72c5e69ac7bf60f5f747
jverbosk <jverbosk@gmail.com>
2017-01-26 00:31:01 -0500

    added placeholder text from multiplication function and test to division function and test, and wrote first division test

--- division.rb
+++ division.rb
@@ -1,1 +1,44 @@
 # Function to divide an indefinite amount of numbers
+
+def divide(*number)
+  # test_array = []
+  # decimal_places = 0
+  # product = 1
+  # if number.length == 0
+  #   test_array.push(false)
+  # end
+  # number.each do |n|
+  #   if (n.class != Fixnum) && (n.class != Float)
+  #     test_array.push(false)
+  #   end
+  # end
+  # if test_array.length > 0
+  #   return false
+  # else
+  #   number.each do |n|
+  #     if n.class == Float
+  #       # If a number is a float, accumulate the number of decimal places
+  #       decimal_places += n.to_s.split('.').last.size
+  #       product *= n
+  #     else
+  #       product *= n
+  #     end
+  #   end
+  # end
+  # return product.round(decimal_places)
+end
+
+# Sandbox testing
+# puts multiply(10, 15)  # 150
+# puts multiply(-8, -9)  # 72
+# puts multiply(-11, 7)  # -77
+# puts multiply(4, 13, 0, 23, 9)  # 0
+# puts multiply(5)  # 5
+# puts multiply(0)  # 0
+# puts multiply()  # false
+# puts multiply(5.2, 8)  # 41.6
+# puts multiply(3.1, 4.1)  # 12.71
+# puts multiply(3.11, 4.11)  # 12.7821
+# puts multiply("a", 3)  # false
+# puts multiply(3, 5, 7, 9, 11)  # 10,395
+# puts multiply(2.2, 4.4, 6.6, 8.8)  # 562.2144
--- test_division.rb
+++ test_division.rb
@@ -5,9 +5,71 @@ require_relative "division.rb"
 
 class TestDivision < Minitest::Test
 
-  def test_1_
-    results = 
-    assert_equal(, results)
+  def test_1_dividing_two_positive_integers
+    results = divide(6, 3)
+    assert_equal(2, results)
   end
+
+  # def test_2_multiplying_two_negative_integers
+  #   results = multiply(-2, -4)
+  #   assert_equal(8, results)
+  # end
+
+  # def test_3_multiplying_one_positive_and_one_negative_integer
+  #   results = multiply(3, -4)
+  #   assert_equal(-12, results)
+  # end
+
+  # def test_4_multiplying_with_zero
+  #   results = multiply(1, 2, 3, 4, 5, 0, 6, 7, 8, 9, 10)
+  #   assert_equal(0, results)
+  # end
+
+  # def test_5_single_argument_17
+  #   results = multiply(17)
+  #   assert_equal(17, results)
+  # end
+
+  # def test_6_single_argument_0
+  #   results = multiply(0)
+  #   assert_equal(0, results)
+  # end
+
+  # def test_7_no_argument
+  #   results = multiply()
+  #   assert_equal(false, results)
+  # end
+
+  # # Playing with a real calculator, product of floats has combined number of decimal places
+  # # Ex: 3.1 & 4.1 > two decimal places, 3.11, 4.11 > four decimal places
+  # def test_8_multiplying_floats_one_decimal_place
+  #   results = multiply(3.1, 4.1)
+  #   assert_equal(12.71, results)
+  # end
+
+  # def test_9_multiplying_floats_two_decimal_places
+  #   results = multiply(3.11, 4.11)
+  #   assert_equal(12.7821, results)
+  # end
+
+  # def test_10_multiplying_an_integer_and_a_float
+  #   results = multiply(3.526, 9)
+  #   assert_equal(31.734, results)
+  # end
+
+  # def test_11_fail_on_non_numbers
+  #   results = multiply("a", 3)
+  #   assert_equal(false, results)
+  # end
+
+  # def test_12_passing_more_than_2_integers
+  #   results = multiply(1, 2, 3, 4, 5)
+  #   assert_equal(120, results)
+  # end
+
+  # def test_13_passing_more_than_2_floats
+  #   results = multiply(1.1, 2.2, 3.3, 4.4, 5.5)
+  #   assert_equal(193.26120, results)
+  # end
 
 end
